8a6d56a451cd3ef98c71925984784f09fcc70363
patrick-gordon <patrickgordon42@yahoo.com>
2020-06-12 12:02:28 -0500

    MVP

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue, Stack
 
 import random
 from ast import literal_eval
@@ -28,6 +29,74 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+def add_to_visited(room_id, visited):
+    if room_id not in visited:
+        visited[room_id] = {'n': '?', 's': '?', 'e': '?', 'w': '?'}
+    return visited
+
+def reverse_direction(direction):
+    if direction == "n":
+        return "s"
+    if direction == "s":
+        return "n"
+    if direction == "e":
+        return "w"
+    if direction == "w":
+        return "e"
+
+def possible_exits(room_id, player, visited):
+    choices = player.current_room.get_exits()
+    possible_choices = []
+    for direction, target_room_id in visited[room_id].items():
+        if direction in choices and target_room_id == '?':
+            possible_choices.append(direction)
+    return possible_choices
+
+
+def traverse_all_rooms(player):
+    visited = {}
+    visited_path = []
+    queue = Queue()
+    queue.enqueue(player.current_room.id)
+
+    while queue.size() > 0:
+        if len(visited) == len(world.rooms):
+            return visited
+        
+        room_id = queue.dequeue()
+        visited = add_to_visited(room_id, visited)
+        
+        
+        if len(visited_path) > 0:
+            prev_room_id = visited_path[-1][0]
+            prev_room_dir = reverse_direction(visited_path[-1][1])
+            visited[room_id][prev_room_dir] = prev_room_id
+        
+        choices = possible_exits(room_id, player, visited)
+        
+        if choices:
+            choice = random.choice(choices)
+            player.travel(choice)
+            visited[room_id][choice] = player.current_room.id
+            visited_path.append([room_id, choice])
+            
+        else:
+            choice = prev_room_dir
+            player.travel(choice)
+            if visited_path:
+                visited_path.pop()
+            else:
+                traversal_path.append(choice)
+                return visited
+        
+        traversal_path.append(choice)
+        queue.enqueue(player.current_room.id)
+        print(visited_path)
+
+
+player = Player(world.starting_room)
+traverse_all_rooms(player)
 
 
 
